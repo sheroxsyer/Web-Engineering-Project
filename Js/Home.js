3e68523e061179c3c478d089851c07f004ac645d
sheroxsyer <innocentsheri2001@gmail.com>
2023-03-26 17:59:52 +0500

    Files

---
+++ Js/Home.js
@@ -0,0 +1,41 @@
+<html>
+<head>
+<title>
+<b>StudyEase</b>
+</title> 
+
+<body>
+SECURE YOUR CAREER
+Welcome to ONLINE COARSE WEBSITE
+
+<h1><i>Courses</h1></i>
+<p><em>Education</em> is both the act of teaching knowledge to others and the act of receiving knowledge from someone else. Education also refers to the knowledge received through schooling or instruction and to the institution of teaching as a whole</p>
+<p>This will help you to study any coarse of any subject .Pick any one of coarses:</p><br/>
+<dl>
+<dt>Education</dt>
+<dd>Education is the medium that gives us the skills, techniques, information and knowledge to know, understand and respect the duties we have towards our society, families and nation. Therefore, the magnitude of the importance of education in life is huge as well as multifold.</dd>
+</dl>
+<h2>Categories</h2><br/>
+<ol>
+<li>Business</li>
+<li>Computing</li>
+<li>Mathematics</li>
+<li>Physics</li>
+<li>Chemistry</li>
+<li>Biology</li>
+</ol>
+<ul>
+<li>Class 9<sup>th</sup> </li>
+<li>Class 10<sup>th</sup></li>
+<li>Class 11<sup>th</sup></li>
+<li>Class 12<sup>th</sup></li>
+</ul>
+<br/>
+<address>
+<b>CONTACT US</b>
+<p><a href="p200480@pwr.nu.edu.pk"></a></p>
+<p>NUCES FAST Hayatabad phase 1 Peshawar. </p>
+</address>
+</body>
+</head>
+</html>
